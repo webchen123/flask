a62417d775608e200fb216b18142a7eda8a3f694
webchen <webchen123@163.com>
2018-08-24 12:57:38 +0800

    test

--- first.py
+++ first.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask,url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'hello world'
+def index():pass
+@app.route('/login')
+def login():pass
+@app.route('/user/<username>')
+def profile(username):pass
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login',next='/'))
+    print(url_for('profile',username='john deo'))
+
+
 if __name__=='__main__':
-    app.run()
+    app.run(debug = True)
