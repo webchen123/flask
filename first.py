075354087459d3c6e9ad22e8799bbbedfed3b042
webchen <webchen123@163.com>
2018-08-29 16:42:25 +0800

    first hello world

--- first.py
+++ first.py
@@ -1,19 +1,27 @@
-from flask import Flask,url_for
+from flask import Flask,render_template,request
 app = Flask(__name__)
-
-@app.route('/')
-def index():pass
-@app.route('/login')
-def login():pass
-@app.route('/user/<username>')
-def profile(username):pass
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login',next='/'))
-    print(url_for('profile',username='john deo'))
-
-
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html',name=name)
+@app.route('/login',methods=['POST','GET'])
+def login():
+    error = None
+    if request.method=='POST':
+        if valid_login(request.form['username'],request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Ivalid username/password'
+    return render_template('login.html',error=error)
+def log_the_user_in(name):
+    return render_template('hello.html',name=name)
+def valid_login(name,password):
+    if name=='abc' and password=='123':
+        return True
+    else:
+        return False
 if __name__=='__main__':
     app.run(debug = True)
+
+
+
